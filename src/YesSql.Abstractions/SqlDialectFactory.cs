b79993117d456c84cfb38fe8fbd7181160701b52
me <me@me.pl>
2019-08-15 19:43:50 +0200

    New changes

--- src/YesSql.Abstractions/SqlDialectFactory.cs
+++ src/YesSql.Abstractions/SqlDialectFactory.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Concurrent;
-using System.Data.Common;
+using System.Data.Common;;;;;
 
 namespace YesSql
 {
@@ -21,6 +21,8 @@ namespace YesSql
             {
                 throw new ArgumentException("Unknown connection name: " + connectionName);
             }
+
+
 
             return dialect;
         }
